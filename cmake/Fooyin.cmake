1e28f394f4e1e8d4283446878c4af1dac34198b2
ludouzi <LukeT1@proton.me>
2022-07-23 13:23:51 +0100

    Build libraries/plugin in common directory
    Also build executable in top level bin directory

--- cmake/Fooyin.cmake
+++ cmake/Fooyin.cmake
@@ -4,6 +4,10 @@ set(FOOYIN_BINARY_DIR "${CMAKE_INSTALL_BINDIR}")
 set(FOOYIN_LIBRARY_DIR "${CMAKE_INSTALL_LIBDIR}/fooyin")
 set(FOOYIN_PLUGIN_DIR "${FOOYIN_LIBRARY_DIR}/plugins")
 
+set(FOOYIN_BINARY_OUTPUT_DIR ${PROJECT_BINARY_DIR}/${FOOYIN_BINARY_DIR})
+set(FOOYIN_LIBRARY_OUTPUT_DIR ${PROJECT_BINARY_DIR}/${FOOYIN_LIBRARY_DIR})
+set(FOOYIN_PLUGIN_OUTPUT_DIR ${PROJECT_BINARY_DIR}/${FOOYIN_PLUGIN_DIR})
+
 macro(fooyin_add_plugin plugin_name)
     set(CMAKE_AUTOUIC ON)
     set(CMAKE_AUTOMOC ON)
@@ -23,25 +27,15 @@ macro(fooyin_add_plugin plugin_name)
    target_link_libraries(${plugin_name} PRIVATE ${plugin_PLUGIN_DEPENDS})
    target_link_libraries(${plugin_name} PRIVATE ${plugin_DEPENDS})
 
-   set_target_properties(${plugin_name} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${FOOYIN_PLUGIN_DIR}")
-   set_target_properties(${plugin_name} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${FOOYIN_PLUGIN_DIR}")
-   set_target_properties(${plugin_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${FOOYIN_BINARY_DIR}")
-   set_target_properties(${plugin_name} PROPERTIES PREFIX "")
+   set_target_properties(${plugin_name} PROPERTIES
+       ARCHIVE_OUTPUT_DIRECTORY "${FOOYIN_PLUGIN_DIR}"
+       LIBRARY_OUTPUT_DIRECTORY "${FOOYIN_PLUGIN_DIR}"
+       RUNTIME_OUTPUT_DIRECTORY "${FOOYIN_BINARY_DIR}"
+       PREFIX ""
+       LIBRARY_OUTPUT_DIRECTORY ${FOOYIN_PLUGIN_OUTPUT_DIR}
+   )
 
    target_include_directories(${plugin_name} INTERFACE ${CMAKE_CURRENT_SOURCE_DIR})
 
    install(TARGETS ${plugin_name} DESTINATION ${FOOYIN_PLUGIN_DIR})
 endmacro(fooyin_add_plugin)
-
-macro(fooyin_add_shared_library library_name)
-    set_target_properties(${library_name} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${FOOYIN_LIBRARY_DIR}")
-    set_target_properties(${library_name} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${FOOYIN_LIBRARY_DIR}")
-    set_target_properties(${library_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${FOOYIN_BINARY_DIR}")
-    set_target_properties(${library_name} PROPERTIES
-      LIBRARY_OUTPUT_DIRECTORY ${LIBRARY_BUILD_TARGET}
-    )
-
-    target_include_directories(${PROJECT_NAME} INTERFACE ${CMAKE_CURRENT_SOURCE_DIR})
-    target_compile_definitions(${PROJECT_NAME} PRIVATE PLUGINSYSTEM_LIBRARY)
-    install(TARGETS ${PROJECT_NAME} DESTINATION ${FOOYIN_LIBRARY_DIR})
-endmacro(fooyin_add_shared_library)
