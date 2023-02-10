90203a1a4628901faa5ef8f2d2dac05d7b940e07
ludouzi <LukeT1@proton.me>
2023-02-10 22:32:44 +0000

    Remove unused helper functions

--- src/utils/helpers.h
+++ src/utils/helpers.h
@@ -24,34 +24,6 @@
 #include <vector>
 
 namespace Utils {
-template <typename T>
-class asRange
-{
-public:
-    explicit asRange(T& data)
-        : m_data{data}
-    { }
-
-    auto begin()
-    {
-        return m_data.keyValueBegin();
-    }
-
-    auto end()
-    {
-        return m_data.keyValueEnd();
-    }
-
-private:
-    T& m_data;
-};
-
-template <typename E>
-constexpr auto toUnderlying(E e) noexcept
-{
-    return static_cast<std::underlying_type_t<E>>(e);
-}
-
 template <typename C, typename F>
 constexpr int getIndex(C c, F f)
 {
@@ -74,21 +46,5 @@ template <typename C, typename F>
 constexpr bool hasKey(C c, F f)
 {
     return c.count(f);
-}
-
-template <typename C, typename S, typename D>
-constexpr void joinSkipEmpty(C c, S& s, D d)
-{
-    int j{0};
-    for(const auto& str : c) {
-        if(str.isEmpty()) {
-            continue;
-        }
-        if(j) {
-            s.append(d);
-        }
-        s.append(str);
-        ++j;
-    }
 }
 } // namespace Utils
