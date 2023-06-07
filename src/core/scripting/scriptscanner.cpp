1082b83653852d15df01b20f72f10ca5200797fd
Luke Taylor <LukeT1@proton.me>
2023-06-07 15:40:59 +0100

    [core] ScriptScanner: Don't parse ']' as literal
    Fixes bug wherein a conditional which includes literal chars before
    being terminated is parsed as a literal, resulting in the entire script
    evaluating to false.

--- src/core/scripting/scriptscanner.cpp
+++ src/core/scripting/scriptscanner.cpp
@@ -23,7 +23,8 @@ namespace Fy::Core::Scripting {
 bool isLiteral(QChar ch)
 {
     return (ch != TokComma && ch != TokQuote && ch != TokLeftParen && ch != TokRightParen && ch != TokLeftSquare
-            && ch != TokFunc && ch != TokVar && ch != TokLeftAngle && ch != TokRightAngle && ch != TokEscape);
+            && ch != TokRightSquare && ch != TokFunc && ch != TokVar && ch != TokLeftAngle && ch != TokRightAngle
+            && ch != TokEscape);
 }
 
 void Scanner::setup(const QString& input)
