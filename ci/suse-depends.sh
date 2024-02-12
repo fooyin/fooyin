1e885f34f5910b4cc9f21429fc7e516d1147446c
Luke Taylor <LukeT1@proton.me>
2024-02-12 21:25:26 +0000

    Update depends

--- ci/suse-depends.sh
+++ ci/suse-depends.sh
@@ -2,7 +2,6 @@
 
 source ci/setup.sh
 
-$SUDO zypper -n remove busybox-which
 $SUDO zypper -n install \
    lsb-release \
    rpm-build \
@@ -13,7 +12,12 @@ $SUDO zypper -n install \
    alsa-devel \
    taglib-devel \
    qt6-base-devel  \
-   qt6-tools \
+   qt6-gui-devel \
+   qt6-widgets-devel \
+   qt6-concurrent-devel \
+   qt6-network-devel \
+   qt6-sql-devel \
+   qt6-linguist-devel \
    ffmpeg-5-libavcodec-devel \
    ffmpeg-5-libavutil-devel \
    ffmpeg-5-libavfilter-devel \
