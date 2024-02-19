b553250c5e252b96f7b04416b62a541b2d8913eb
Luke Taylor <LukeT1@proton.me>
2024-02-19 21:04:40 +0000

    [core] Restore mutex guard when using pull output callback

--- src/core/engine/ffmpeg/ffmpegrenderer.cpp
+++ src/core/engine/ffmpeg/ffmpegrenderer.cpp
@@ -174,7 +174,7 @@ struct FFmpegRenderer::Private
             return 0;
         }
 
-        // const std::lock_guard<std::mutex> lock(bufferMutex);
+        const std::lock_guard<std::mutex> lock(bufferMutex);
 
         const int samplesWritten = writeAudioSamples(samples);
         const int sstride        = tempBuffer.format().bytesPerFrame();
