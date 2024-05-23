4520bb609305123a7638a6994245a18214eea79a
Luke Taylor <LukeT1@proton.me>
2024-05-23 21:43:50 +0100

    [CI] Try to add pipewire to ubuntu depends

--- ci/ubuntu-depends.sh
+++ ci/ubuntu-depends.sh
@@ -7,6 +7,6 @@ echo 'debconf debconf/frontend select Noninteractive' | $SUDO debconf-set-select
 $SUDO apt-get update -qq
 $SUDO apt-get install -y \
         g++ git cmake pkg-config ninja-build debhelper lsb-release libglu1-mesa-dev libxkbcommon-dev dpkg-dev dh-make \
-        libasound2-dev libtag1-dev \
+        libasound2-dev libpipewire-0.3-dev libtag1-dev \
         qt6-base-dev libqt6svg6-dev qt6-tools-dev qt6-tools-dev-tools qt6-l10n-tools \
         libavcodec-dev libavformat-dev libavutil-dev
