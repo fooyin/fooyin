7e5f1b563961eedd86b9e41fd9c39e3e9ba4712c
Luke Taylor <LukeT1@proton.me>
2024-05-28 08:30:29 +0100

    [gui] Playlist: Fix moveCursor behaviour
    Selecting/moving to the current index was disabled so it wouldn't automatically scroll to the playing track whenever its playcount was incremented
    Now that we only update the rows data instead of removing and reinserting, we can re-enable the default QAbstractItemView behaviour
    
    Addresses #121

--- src/gui/playlist/playlistview.cpp
+++ src/gui/playlist/playlistview.cpp
@@ -2496,6 +2496,8 @@ void PlaylistView::setSelection(const QRect& rect, QItemSelectionModel::Selectio
 
 void PlaylistView::currentChanged(const QModelIndex& current, const QModelIndex& previous)
 {
+    QAbstractItemView::currentChanged(current, previous);
+
     if(previous.isValid()) {
         viewport()->update(p->visualRect(previous, RectRule::FullRow, false));
     }
