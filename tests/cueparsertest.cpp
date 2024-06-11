42c3f454070b93a76327bb203f94f78dbd44e2ca
Luke Taylor <LukeT1@proton.me>
2024-06-11 18:37:10 +0100

    [tests] Add single file cue test

---
+++ tests/cueparsertest.cpp
@@ -0,0 +1,58 @@
+/*
+ * Fooyin
+ * Copyright © 2024, Luke Taylor <LukeT1@proton.me>
+ *
+ * Fooyin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fooyin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fooyin.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "core/playlist/parsers/cueparser.h"
+#include "testutils.h"
+
+#include <core/track.h>
+
+#include <gtest/gtest.h>
+
+#include <QDir>
+
+namespace Fooyin::Testing {
+class CueParserTest : public ::testing::Test
+{
+protected:
+    std::unique_ptr<PlaylistParser> m_parser{std::make_unique<CueParser>()};
+};
+
+TEST_F(CueParserTest, NoCue)
+{
+    const auto tracks = m_parser->readPlaylist(QStringLiteral(""), false);
+    EXPECT_EQ(0, tracks.size());
+}
+
+TEST_F(CueParserTest, SingleCue)
+{
+    const TempResource file{QStringLiteral(":/cue/singlefiletest.cue")};
+    file.checkValid();
+
+    const auto tracks = m_parser->readPlaylist(QStringLiteral(":/cue/singlefiletest.cue"), false);
+    ASSERT_EQ(2, tracks.size());
+
+    EXPECT_EQ(1991, tracks.at(0).year());
+    EXPECT_EQ(u"Alternative", tracks.at(0).genre());
+    EXPECT_EQ(u"Loveless", tracks.at(0).album());
+    EXPECT_EQ(u"Only Shallow", tracks.at(0).title());
+
+    EXPECT_EQ(1, tracks.at(1).discNumber());
+    EXPECT_EQ(2, tracks.at(1).trackNumber());
+}
+} // namespace Fooyin::Testing
---
+++ tests/data/cue/singlefiletest.cue
@@ -0,0 +1,16 @@
+REM GENRE Alternative
+REM DATE 1991
+REM DISCID 860B640B
+REM DISCNUMBER 1
+REM COMMENT "ExactAudioCopy v0.95b4"
+PERFORMER "My Bloody Valentine"
+TITLE "Loveless"
+FILE "My Bloody Valentine - Loveless.wav" WAVE
+  TRACK 01 AUDIO
+    TITLE "Only Shallow"
+    PERFORMER "My Bloody Valentine"
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
+    TITLE "Loomer"
+    PERFORMER "My Bloody Valentine"
+    INDEX 01 04:17:52
