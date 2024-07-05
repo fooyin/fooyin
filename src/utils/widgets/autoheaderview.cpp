2e419323ec2657dc10713f6574df0525e17964cb
Luke Taylor <LukeT1@proton.me>
2024-07-05 10:46:03 +0100

    [utils] AutoHeaderView: Restore toggling of stretch

--- src/utils/widgets/autoheaderview.cpp
+++ src/utils/widgets/autoheaderview.cpp
@@ -478,8 +478,7 @@ void AutoHeaderView::addHeaderContextMenu(QMenu* menu, const QPoint& pos)
     auto* toggleStretch = new QAction(tr("Auto-size sections"), menu);
     toggleStretch->setCheckable(true);
     toggleStretch->setChecked(p->m_stretchEnabled);
-    // QObject::connect(toggleStretch, &QAction::triggered, this, [this]() { setStretchEnabled(!p->m_stretchEnabled);
-    // });
+    QObject::connect(toggleStretch, &QAction::triggered, this, [this]() { setStretchEnabled(!p->m_stretchEnabled); });
     menu->addAction(toggleStretch);
 
     menu->addSeparator();
