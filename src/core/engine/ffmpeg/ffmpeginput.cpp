6d370d33a065410faffacea830ef5c79b3ada210
Luke Taylor <LukeT1@proton.me>
2024-09-23 11:27:06 +0100

    [core] FFmpeg: Extend format blacklist
    And remove unneeded comments

--- src/core/engine/ffmpeg/ffmpeginput.cpp
+++ src/core/engine/ffmpeg/ffmpeginput.cpp
@@ -68,40 +68,60 @@ QStringList fileExtensions(bool allSupported)
         }
     }
 
-    static constexpr std::array extensionBlacklist = {"apc",            // apc (CRYO APC)
-                                                      "aqt", "aqtitle", // aqtitle (AQTitle subtitles)
-                                                      "ass",            // ass (SSA (SubStation Alpha) subtitle)
-                                                      "bin",            // bin (Binary text)
-                                                      "dss",            // dss (Digital Speech Standard (DSS))
-                                                      "dvbsub",         // dvbsub (raw dvbsub)
-                                                      "ffmetadata",     // ffmetadata (FFmpeg metadata in text)
-                                                      "ilbc",           // ilbc (iLBC storage)
-                                                      "image2pipe",     // image2pipe (piped image2 sequence)
-                                                      "jacosub",        // jacosub (JACOsub subtitle format)
-                                                      "txt",            // txt (Text file)
-                                                      "mpl2",           // mpl2 (MPL2 subtitles)
-                                                      "sub", "mpsub",   // mpsub (MPlayer subtitles)
-                                                      "pjs", // pjs (PJS (Phoenix Japanimation Society) subtitles)
-                                                      "smi", "sami",       // sami (SAMI subtitle format)
-                                                      "srt",               // srt (SubRip subtitle)
-                                                      "stl",               // stl (Spruce subtitle format)
-                                                      "sub", "subviewer1", // subviewer1 (SubViewer v1 subtitle format)
-                                                      "sup", // sup (raw HDMV Presentation Graphic Stream subtitles)
-                                                      "vtt", "webvtt", // webvtt (WebVTT subtitle)
-                                                      "bmp_pipe",      // bmp_pipe (piped bmp sequence)
-                                                      "dds_pipe",      // dds_pipe (piped dds sequence)
-                                                      "dpx_pipe",      // dpx_pipe (piped dpx sequence)
-                                                      "exr_pipe",      // exr_pipe (piped exr sequence)
-                                                      "jpeg_pipe",     // jpeg_pipe (piped jpeg sequence)
-                                                      "png_pipe",      // png_pipe (piped png sequence)
-                                                      "tiff_pipe",     // tiff_pipe (piped tiff sequence)
-                                                      // Image formats:
-                                                      "bmp", "jpg", "jpeg", "png", "tif", "tiff", "gif", "ico", "exr"};
+    static constexpr std::array extensionBlacklist{"ans",
+                                                   "apc",
+                                                   "aqt",
+                                                   "aqtitle",
+                                                   "art",
+                                                   "asc",
+                                                   "ass",
+                                                   "bin"
+                                                   "bmp_pipe",
+                                                   "bmp",
+                                                   "dds_pipe",
+                                                   "diz",
+                                                   "dpx_pipe",
+                                                   "dss",
+                                                   "dvbsub",
+                                                   "exr_pipe",
+                                                   "exr",
+                                                   "ffmetadata"
+                                                   "gif",
+                                                   "ice",
+                                                   "ico",
+                                                   "ilbc",
+                                                   "image2pipe",
+                                                   "jacosub",
+                                                   "jpeg_pipe",
+                                                   "jpeg",
+                                                   "jpg",
+                                                   "mpl2",
+                                                   "mpsub",
+                                                   "nfo",
+                                                   "pjs",
+                                                   "png_pipe",
+                                                   "png",
+                                                   "sami",
+                                                   "smi",
+                                                   "srt",
+                                                   "stl",
+                                                   "sub",
+                                                   "sub",
+                                                   "subviewer1",
+                                                   "sup",
+                                                   "tif",
+                                                   "tiff_pipe",
+                                                   "tiff",
+                                                   "txt",
+                                                   "vt",
+                                                   "vtt",
+                                                   "webvtt"};
 
     for(const auto* ext : extensionBlacklist) {
         extensions.removeAll(QLatin1String{ext});
     }
     extensions.removeDuplicates();
+    qDebug() << extensions;
 
     return extensions;
 }
