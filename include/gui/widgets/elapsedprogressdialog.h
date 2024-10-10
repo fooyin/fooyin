7678bde70be6e3ef0a12e349bdaec35bd9ae6f39
Luke Taylor <LukeT1@proton.me>
2024-10-10 11:26:42 +0100

    [gui] Add progress dialog with extra progress info

---
+++ include/gui/widgets/elapsedprogressdialog.h
@@ -0,0 +1,64 @@
+/*
+ * Fooyin
+ * Copyright © 2024, Luke Taylor <LukeT1@proton.me>
+ *
+ * Fooyin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fooyin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fooyin.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include "fygui_export.h"
+
+#include <utils/timer.h>
+
+#include <QDialog>
+
+class QLabel;
+class QProgressBar;
+
+namespace Fooyin {
+class ElidedLabel;
+
+class FYGUI_EXPORT ElapsedProgressDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ElapsedProgressDialog(const QString& labelText, const QString& cancelButtonText, int minimum, int maximum,
+                          QWidget* parent = nullptr);
+
+    [[nodiscard]] int value() const;
+    void setValue(int value);
+
+    void setText(const QString& text);
+
+    [[nodiscard]] bool wasCancelled() const;
+
+    [[nodiscard]] QSize sizeHint() const override;
+
+private:
+    void updateStatus();
+
+    ElidedLabel* m_label;
+    QProgressBar* m_progressBar;
+    bool m_wasCancelled;
+
+    QTimer* m_updateTimer;
+    Timer m_elapsedTimer;
+    QLabel* m_elapsedLabel;
+    QLabel* m_remainingLabel;
+    QPushButton* m_cancelButton;
+};
+} // namespace Fooyin
---
+++ src/gui/widgets/elapsedprogressdialog.cpp
@@ -0,0 +1,124 @@
+/*
+ * Fooyin
+ * Copyright © 2024, Luke Taylor <LukeT1@proton.me>
+ *
+ * Fooyin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fooyin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fooyin.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <gui/widgets/elapsedprogressdialog.h>
+
+#include <gui/widgets/elidedlabel.h>
+#include <utils/timer.h>
+#include <utils/utils.h>
+
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QTimer>
+
+namespace Fooyin {
+ElapsedProgressDialog::ElapsedProgressDialog(const QString& labelText, const QString& cancelButtonText, int minimum,
+                                             int maximum, QWidget* parent)
+    : QDialog{parent}
+    , m_label{new ElidedLabel(this)}
+    , m_progressBar{new QProgressBar(this)}
+    , m_updateTimer{new QTimer(this)}
+    , m_elapsedLabel{new QLabel(this)}
+    , m_remainingLabel{new QLabel(this)}
+    , m_cancelButton{new QPushButton(this)}
+{
+    auto* statusLayout = new QHBoxLayout();
+    statusLayout->addWidget(m_elapsedLabel);
+    statusLayout->addWidget(m_remainingLabel);
+    statusLayout->addStretch();
+    statusLayout->addWidget(m_cancelButton);
+
+    auto* layout = new QGridLayout(this);
+    layout->addWidget(m_label, 0, 0, 2, 1);
+    layout->addWidget(m_progressBar, 2, 0);
+    layout->addLayout(statusLayout, 3, 0);
+    layout->setRowStretch(1, 1);
+
+    m_label->setElideMode(Qt::ElideMiddle);
+    m_label->setText(labelText);
+    m_cancelButton->setText(cancelButtonText);
+    m_progressBar->setRange(minimum, maximum);
+    m_progressBar->setValue(minimum);
+
+    m_updateTimer->setInterval(1000);
+    QObject::connect(m_updateTimer, &QTimer::timeout, this, &ElapsedProgressDialog::updateStatus);
+    QObject::connect(m_cancelButton, &QAbstractButton::clicked, this, [this]() {
+        m_wasCancelled = true;
+        m_updateTimer->stop();
+        hide();
+    });
+
+    m_updateTimer->start();
+    m_elapsedTimer.reset();
+    updateStatus();
+    show();
+}
+
+int ElapsedProgressDialog::value() const
+{
+    return m_progressBar->value();
+}
+
+void ElapsedProgressDialog::setValue(int value)
+{
+    m_progressBar->setValue(value);
+}
+
+void ElapsedProgressDialog::setText(const QString& text)
+{
+    m_label->setText(text);
+}
+
+bool ElapsedProgressDialog::wasCancelled() const
+{
+    return m_wasCancelled;
+}
+
+QSize ElapsedProgressDialog::sizeHint() const
+{
+    return {400, 150};
+}
+
+void ElapsedProgressDialog::updateStatus()
+{
+    m_elapsedLabel->setText(tr("Time elapsed") + u": " + Utils::msToString(m_elapsedTimer.elapsed(), false));
+
+    const int current = m_progressBar->value();
+    const int max     = m_progressBar->maximum();
+    const int min     = m_progressBar->minimum();
+
+    if(current <= min) {
+        m_remainingLabel->setText(tr("Calculating…"));
+        return;
+    }
+
+    const auto elapsedMs     = static_cast<double>(m_elapsedTimer.elapsed().count());
+    const double elapsedSecs = elapsedMs / 1000.0;
+
+    const int remaining        = max - current;
+    const double remainingSecs = remaining / (current / elapsedSecs);
+    const auto remainingMs     = static_cast<std::chrono::milliseconds>(static_cast<int>(remainingSecs * 1000));
+
+    const QString remainingText = Utils::msToString(remainingMs, false);
+    m_remainingLabel->setText(tr("Estimated") + QStringLiteral(": %1").arg(remainingText));
+}
+} // namespace Fooyin
