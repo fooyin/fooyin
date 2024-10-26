e7f1e245884107980d7b58aa418614b5d35a2640
麋悟BigELK176 <bigelk176@gmail.com>
2024-10-26 02:05:32 +0000

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (1425 of 1425 strings)
    
    Translation: fooyin/fooyin
    Translate-URL: https://hosted.weblate.org/projects/fooyin/fooyin/zh_Hant/

--- data/translations/fooyin_zh_Hant.ts
+++ data/translations/fooyin_zh_Hant.ts
@@ -3171,52 +3171,52 @@
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="47"/>
         <source>Seek</source>
-        <translation type="unfinished"></translation>
+        <translation>尋找</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="48"/>
         <source>Reset Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>重設變更</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="49"/>
         <source>Insert/Update</source>
-        <translation type="unfinished"></translation>
+        <translation>插入/更新</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="50"/>
         <source>Remove</source>
-        <translation type="unfinished">移除</translation>
+        <translation>移除</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="51"/>
         <source>Remove All</source>
-        <translation type="unfinished"></translation>
+        <translation>移除全部</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="57"/>
         <source>Lyrics Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>歌詞編輯器</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="61"/>
         <source>Operation</source>
-        <translation type="unfinished">操作</translation>
+        <translation>操作</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="68"/>
         <source>Timestamps</source>
-        <translation type="unfinished"></translation>
+        <translation>時間戳記</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="139"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>暫停</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricseditor.cpp" line="143"/>
         <source>Play</source>
-        <translation type="unfinished">播放</translation>
+        <translation>播放</translation>
     </message>
 </context>
 <context>
@@ -3338,7 +3338,7 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricsguipage.cpp" line="100"/>
         <source>Unplayed line colour</source>
-        <translation type="unfinished"></translation>
+        <translation>尚未播放線條顏色</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricsguipage.cpp" line="102"/>
@@ -3459,12 +3459,12 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="277"/>
         <source>Saving</source>
-        <translation type="unfinished">儲存</translation>
+        <translation>儲存中</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="278"/>
         <source>Lyrics</source>
-        <translation type="unfinished">歌詞</translation>
+        <translation>歌詞</translation>
     </message>
 </context>
 <context>
@@ -3472,102 +3472,102 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="79"/>
         <source>Manual save</source>
-        <translation type="unfinished"></translation>
+        <translation>手動儲存</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="80"/>
         <source>Autosave</source>
-        <translation type="unfinished"></translation>
+        <translation>自動儲存</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="81"/>
         <source>Autosave after 60 seconds or 1/3 of track duration</source>
-        <translation type="unfinished"></translation>
+        <translation>在曲目60秒或1/3之後自動儲存</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="82"/>
         <source>Save to metadata tag</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存資料標籤</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="83"/>
         <source>Save to directory</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存至目錄</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="84"/>
         <source>Save all</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存全部</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="85"/>
         <source>Synced only</source>
-        <translation type="unfinished"></translation>
+        <translation>僅有已同步的</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="86"/>
         <source>Unsynced only</source>
-        <translation type="unfinished"></translation>
+        <translation>僅有未同步的</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="91"/>
         <source>Collapse duplicate lines</source>
-        <translation type="unfinished"></translation>
+        <translation>縮合重複的行列</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="92"/>
         <source>Save metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存資料</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="94"/>
         <source>Save Scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存方略</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="102"/>
         <source>Save Method</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存方法</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="109"/>
         <source>Autosave Preference</source>
-        <translation type="unfinished"></translation>
+        <translation>自動儲存偏好設定</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="117"/>
         <source>Save Location</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存位置</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="120"/>
         <source>Synced lyric tag</source>
-        <translation type="unfinished"></translation>
+        <translation>已同步的歌詞標籤</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="121"/>
         <source>Unsynced lyric tag</source>
-        <translation type="unfinished"></translation>
+        <translation>未同步的歌詞標籤</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="122"/>
         <source>Directory</source>
-        <translation type="unfinished">目錄</translation>
+        <translation>目錄</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="123"/>
         <source>Filename</source>
-        <translation type="unfinished">檔名</translation>
+        <translation>檔名</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="135"/>
         <source>Save Format</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存格式</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssavingpage.cpp" line="267"/>
         <source>Select Directory</source>
-        <translation type="unfinished">選擇目錄</translation>
+        <translation>選擇目錄</translation>
     </message>
 </context>
 <context>
@@ -3575,12 +3575,12 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="133"/>
         <source>Searching</source>
-        <translation type="unfinished">搜尋中</translation>
+        <translation>搜尋中</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="134"/>
         <source>Lyrics</source>
-        <translation type="unfinished">歌詞</translation>
+        <translation>歌詞</translation>
     </message>
 </context>
 <context>
@@ -3588,47 +3588,47 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="62"/>
         <source>Automatically search for lyrics on starting playback</source>
-        <translation type="unfinished"></translation>
+        <translation>在啟動播放時自動地搜尋歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="63"/>
         <source>Skip remaining sources when lyrics are found</source>
-        <translation type="unfinished"></translation>
+        <translation>當歌詞已找到時忽略保留來源</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="64"/>
         <source>Skip external sources if local lyrics are found</source>
-        <translation type="unfinished"></translation>
+        <translation>如果在本機中找到歌詞，則跳過外部來源</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="68"/>
         <source>Minimum match threshold</source>
-        <translation type="unfinished"></translation>
+        <translation>最小符合閾值</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="70"/>
         <source>Search parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>搜尋參數</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="76"/>
         <source>Title</source>
-        <translation type="unfinished">標題</translation>
+        <translation>標題</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="78"/>
         <source>Artist</source>
-        <translation type="unfinished">藝人</translation>
+        <translation>藝人</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="80"/>
         <source>Album</source>
-        <translation type="unfinished">專輯</translation>
+        <translation>專輯</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssearchingpage.cpp" line="84"/>
         <source>Only local lyrics will be used if unchecked</source>
-        <translation type="unfinished"></translation>
+        <translation>只有本機歌詞會被使用，若無勾選</translation>
     </message>
 </context>
 <context>
@@ -3636,12 +3636,12 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssourcespage.cpp" line="139"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>來源</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssourcespage.cpp" line="140"/>
         <source>Lyrics</source>
-        <translation type="unfinished">歌詞</translation>
+        <translation>歌詞</translation>
     </message>
 </context>
 <context>
@@ -3649,22 +3649,22 @@
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssourcespage.cpp" line="80"/>
         <source>Lyrics sources</source>
-        <translation type="unfinished"></translation>
+        <translation>歌詞來源</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssourcespage.cpp" line="82"/>
         <source>Lyrics will be searched for in the above order.</source>
-        <translation type="unfinished"></translation>
+        <translation>將會進行歌詞搜尋，按由上列順序。</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssourcespage.cpp" line="84"/>
         <source>Metadata tags</source>
-        <translation type="unfinished"></translation>
+        <translation>資料標籤</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/settings/lyricssourcespage.cpp" line="86"/>
         <source>Local files</source>
-        <translation type="unfinished"></translation>
+        <translation>本機檔案</translation>
     </message>
 </context>
 <context>
@@ -3672,17 +3672,17 @@
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="137"/>
         <source>Artist</source>
-        <translation type="unfinished">藝人</translation>
+        <translation>藝人</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="137"/>
         <source>Album</source>
-        <translation type="unfinished">專輯</translation>
+        <translation>專輯</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="137"/>
         <source>Title</source>
-        <translation type="unfinished">標題</translation>
+        <translation>標題</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="142"/>
@@ -3692,37 +3692,37 @@
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="166"/>
         <source>Select lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>選擇歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="177"/>
         <source>Search for lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>搜尋歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="178"/>
         <source>Search for lyrics for the current track</source>
-        <translation type="unfinished"></translation>
+        <translation>搜尋歌詞目前曲目</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="189"/>
         <source>Edit lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>編輯歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="190"/>
         <source>Open editor for the current lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>開啟編輯器目前歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="195"/>
         <source>Save lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>儲存歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="196"/>
         <source>Save lyrics using current settings</source>
-        <translation type="unfinished"></translation>
+        <translation>使用目前設定儲存歌詞</translation>
     </message>
     <message>
         <location filename="../../src/plugins/lyrics/lyricswidget.cpp" line="205"/>
@@ -7411,6 +7411,7 @@ for waveform data. Higher values will result
 in a more accurate and detailed waveform at the 
 cost of using more disk space in the cache.</source>
         <translation>用於波形資料的樣本數量 (每聲道)。
+
 較高的值會產生更準確細緻的波形，
 但是代價是佔用更多磁碟快取空間。</translation>
     </message>
