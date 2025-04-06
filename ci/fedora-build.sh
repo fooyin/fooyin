860b2b3ee45374136437392fbd7293572e6f180c
Christopher Snowhill <kode54@gmail.com>
2025-04-06 04:47:29 -0700

    Revert "[CI] Fix building for Fedora 42"
    This reverts commit 9d7bb34a8fa5c0323392c7332a224eb02fc31353.
    
    Upstream has fixed libvgm with CMake 4.0.

--- ci/fedora-build.sh
+++ ci/fedora-build.sh
@@ -5,8 +5,7 @@ export FOOYIN_DIR=$PWD
 cmake -S "$FOOYIN_DIR" \
   -G Ninja \
   -B build \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_LIBVGM=OFF
+  -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build
 cd build
