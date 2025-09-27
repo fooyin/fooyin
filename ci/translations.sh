7c38e5e35093813bfc92bb2e3f4d26d824c79709
Luke Taylor <luket@pm.me>
2025-09-27 13:55:06 +0100

    [CI] Add workflow to update translation sources

---
+++ ci/translations.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eux
+
+source ci/setup.sh
+
+$SUDO apt-get update -qq
+$SUDO apt-get install -y \
+    gnupg \
+    qttools5-dev-tools
+
+FOOYIN_DIR="$PWD"
+
+lupdate "$FOOYIN_DIR/src" -no-obsolete -I "$FOOYIN_DIR/include" -ts "$FOOYIN_DIR/data/translations/fooyin_*.ts"
+lupdate "$FOOYIN_DIR/src" -no-obsolete -I "$FOOYIN_DIR/include" -ts -pluralonly "$FOOYIN_DIR/data/translations/fooyin_en_GB.ts"
