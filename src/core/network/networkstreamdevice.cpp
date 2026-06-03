d760ec1aa8969d2ba29d279ee6419ec126c867da
Luke Taylor <luket@pm.me>
2026-06-03 23:10:02 +0100

    [all] Update copyright email in licence header

--- src/core/network/networkstreamdevice.cpp
+++ src/core/network/networkstreamdevice.cpp
@@ -1,6 +1,6 @@
 /*
  * Fooyin
- * Copyright © 2026, Luke Taylor <luket@pm.me>>
+ * Copyright © 2026, Luke Taylor <luket@pm.me>
  *
  * Fooyin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/plugins/rgscanner/ffmpegscanner.cpp
+++ src/plugins/rgscanner/ffmpegscanner.cpp
@@ -1,6 +1,6 @@
 /*
  * Fooyin
- * Copyright © 2024, Luke Taylor <LukeT1@proton.me>
+ * Copyright © 2024, Luke Taylor <luket@pm.me>
  * Copyright © 2024, Gustav Oechler <gustavoechler@gmail.com>
  *
  * Fooyin is free software: you can redistribute it and/or modify
