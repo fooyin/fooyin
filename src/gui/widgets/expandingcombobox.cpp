853a38542ed03b518114504f27f5a24dfb2493d9
Luke Taylor <luket@pm.me>
2026-06-05 14:02:29 +0100

    [gui] ExpandingComboBox: Use 0 to reset min size

--- src/gui/widgets/expandingcombobox.cpp
+++ src/gui/widgets/expandingcombobox.cpp
@@ -43,7 +43,7 @@ void ExpandingComboBox::setResizeToCurrentEnabled(bool enabled)
         resizeToFitCurrent();
     }
     else {
-        setMinimumSize({});
+        setMinimumSize(0, 0);
     }
 }
 
