4c19c03ff8bb6f1d8258312fba9e9227ea51a954
Luke Taylor <luket@pm.me>
2026-09-07 15:06:32 +0100

    [gui] Update copyright email address

--- src/gui/editablelayout_p.h
+++ src/gui/editablelayout_p.h
@@ -1,6 +1,6 @@
 /*
  * Fooyin
- * Copyright © 2025, Luke Taylor <LukeT1@proton.me>
+ * Copyright © 2025, Luke Taylor <luket@pm.me>
  *
  * Fooyin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
