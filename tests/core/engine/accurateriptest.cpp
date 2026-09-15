a2a24d92f3d1b6d1a8add08b17708ccf1970ccd5
Luke Taylor <luket@pm.me>
2026-09-15 20:15:52 +0100

    [all] Normalise license headers

--- tests/core/engine/accurateriptest.cpp
+++ tests/core/engine/accurateriptest.cpp
@@ -14,6 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Fooyin.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <core/engine/verification/accuraterip.h>
