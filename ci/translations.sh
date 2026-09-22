f791bdeb1f1023a7c7ccbf451e10eaba2eb9f94b
Luke Taylor <luket@pm.me>
2026-09-22 19:41:53 +0100

    [CI] Install translation dependencies before checkout

--- ci/translations.sh
+++ ci/translations.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -eux
 
-pacman -Syu --noconfirm \
-    git \
-    gnupg \
-    qt6-declarative \
-    qt6-tools
-
 /usr/bin/lupdate6 src -no-obsolete -I include -ts data/translations/fooyin_*.ts
 /usr/bin/lupdate6 src -no-obsolete -I include -ts -pluralonly data/translations/fooyin_en_GB.ts
