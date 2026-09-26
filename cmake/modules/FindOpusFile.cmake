576759f41d8d97c6f819750068429310b899a482
Luke Taylor <luket@pm.me>
2026-09-26 19:43:28 +0100

    [cmake] Fix OpusFile dependency handling

--- cmake/modules/FindOpusFile.cmake
+++ cmake/modules/FindOpusFile.cmake
@@ -23,6 +23,14 @@ find_path(
     DOC "libopusfile include directory"
 )
 
+find_path(
+    OpusFile_OPUS_INCLUDE_DIR
+    NAMES opus_multistream.h
+    HINTS ${OpusFile_INCLUDE_HINTS}
+    PATH_SUFFIXES opus include/opus
+    DOC "libopus include directory"
+)
+
 unset(OpusFile_INCLUDE_HINT)
 unset(OpusFile_INCLUDE_HINTS)
 unset(OpusFile_INCLUDE_PARENT)
@@ -34,22 +42,47 @@ find_library(
     DOC "libopusfile library"
 )
 
+find_library(
+    OpusFile_OGG_LIBRARY
+    NAMES ogg
+    HINTS ${PC_OpusFile_LIBDIR} ${PC_OpusFile_LIBRARY_DIRS} ${PC_OpusFile_STATIC_LIBRARY_DIRS}
+    DOC "libogg library"
+)
+
+find_library(
+    OpusFile_OPUS_LIBRARY
+    NAMES opus
+    HINTS ${PC_OpusFile_LIBDIR} ${PC_OpusFile_LIBRARY_DIRS} ${PC_OpusFile_STATIC_LIBRARY_DIRS}
+    DOC "libopus library"
+)
+
 set(OpusFile_VERSION ${PC_OpusFile_VERSION})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     OpusFile
-    REQUIRED_VARS OpusFile_LIBRARY OpusFile_ROOT_INCLUDE_DIR
+    REQUIRED_VARS OpusFile_LIBRARY
+                  OpusFile_OGG_LIBRARY
+                  OpusFile_OPUS_LIBRARY
+                  OpusFile_ROOT_INCLUDE_DIR
+                  OpusFile_OPUS_INCLUDE_DIR
     VERSION_VAR OpusFile_VERSION
 )
 
-mark_as_advanced(OpusFile_ROOT_INCLUDE_DIR OpusFile_LIBRARY)
+mark_as_advanced(
+    OpusFile_ROOT_INCLUDE_DIR
+    OpusFile_OPUS_INCLUDE_DIR
+    OpusFile_LIBRARY
+    OpusFile_OGG_LIBRARY
+    OpusFile_OPUS_LIBRARY
+)
 
 if(OpusFile_FOUND AND NOT TARGET OpusFile::OpusFile)
     add_library(OpusFile::OpusFile UNKNOWN IMPORTED GLOBAL)
     set_target_properties(
         OpusFile::OpusFile
         PROPERTIES IMPORTED_LOCATION "${OpusFile_LIBRARY}"
-                   INTERFACE_INCLUDE_DIRECTORIES "${OpusFile_ROOT_INCLUDE_DIR};${OpusFile_ROOT_INCLUDE_DIR}/opus"
+                   INTERFACE_INCLUDE_DIRECTORIES "${OpusFile_ROOT_INCLUDE_DIR};${OpusFile_OPUS_INCLUDE_DIR}"
+                   INTERFACE_LINK_LIBRARIES "${OpusFile_OGG_LIBRARY};${OpusFile_OPUS_LIBRARY}"
     )
 endif()
